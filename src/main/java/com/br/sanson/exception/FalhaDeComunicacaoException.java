1366e5cab180735e77aa63106939e7a1a98602cf
Eduardo Sanson <eduardo.sanson@m4u.com.br>
2017-02-08 18:29:19 -0200

    aprimorando o docker

--- src/main/java/com/br/sanson/exception/FalhaDeComunicacaoException.java
+++ src/main/java/com/br/sanson/exception/FalhaDeComunicacaoException.java
@@ -12,8 +12,8 @@ public class FalhaDeComunicacaoException extends RuntimeException {
 
     private static final Logger logger = Logger.getLogger(FalhaDeComunicacaoException.class);
 
-    public <T> FalhaDeComunicacaoException(Class<T> recurso, int status, Object... params) {
-        super("Erro na chamada " + recurso.getSimpleName() + " para os parametros " + Arrays.toString(params));
+    public <T> FalhaDeComunicacaoException(Class<T> recurso, int status, RuntimeException re,Object... params) {
+        super("Erro na chamada " + recurso.getSimpleName() +  " response code " + status + " para os parametros " + Arrays.toString(params),re);
         logger.error("Erro na chamada " + recurso.getSimpleName() + " response code " + status +" para os parametros " + Arrays.toString(params));
     }
 
